9b721a4b2ba2fb933affe4c6fe5a6bb33074e670
mharris <mharris021@hotmail.com>
2013-07-17 18:35:09 -0500

    rearranged properties

--- src/main/java/com/sf/jake/config/MongoRespositoryConfig.java
+++ src/main/java/com/sf/jake/config/MongoRespositoryConfig.java
@@ -1,5 +1,8 @@
 package com.sf.jake.config;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -13,6 +16,7 @@ import com.mongodb.MongoClientURI;
 @EnableMongoRepositories
 public class MongoRespositoryConfig extends AbstractMongoConfiguration{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MongoRespositoryConfig.class); 
 	
 	@Value(value="${mongo.uri}")
 	private String uri;
@@ -24,7 +28,9 @@ public class MongoRespositoryConfig extends AbstractMongoConfiguration{
 
 	@Override
 	public Mongo mongo() throws Exception {
-		MongoClientURI mongoClientURI = new MongoClientURI(uri);
+		LOGGER.error("Mongo URI:" + uri);
+			MongoClientURI mongoClientURI = new MongoClientURI(uri);
+			
 		MongoClient mongoClient = new MongoClient(mongoClientURI);
 		return mongoClient;
 	}
