5314d3fe8b81365a7e4d7d7bdeabe43559924d0a
mharris <mharris021@hotmail.com>
2013-07-17 23:39:47 -0500

    added quote system

--- src/main/java/com/sf/jake/services/InsuranceQuoteService.java
+++ src/main/java/com/sf/jake/services/InsuranceQuoteService.java
@@ -3,12 +3,14 @@ package com.sf.jake.services;
 import javax.annotation.Resource;
 
 import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
 
 import com.sf.jake.model.CarInsurance;
 import com.sf.jake.model.CarInsuranceQuote;
 import com.sf.jake.model.Vehicle;
 import com.sf.jake.repositories.CarInsuranceRepository;
 
+@Service
 public class InsuranceQuoteService {
 
 	@Resource
